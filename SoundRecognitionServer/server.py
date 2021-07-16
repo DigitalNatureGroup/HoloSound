1c486a1a0d1a913a8a2a6d6797db2e50e68c26cd
Kazuya Izumi <shikibu9419@gmail.com>
2021-07-16 17:19:43 +0900

    Fix importing

--- SoundRecognitionServer/server.py
+++ SoundRecognitionServer/server.py
@@ -1,13 +1,13 @@
 import struct
-from keras.models import load_model
-import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow import get_default_graph
 import numpy as np
 from vggish_input import waveform_to_examples
 import homesounds
 from pathlib import Path
 import time
 import argparse
-import wget
+# import wget
 from helpers import dbFS
 import socket
 
@@ -20,8 +20,8 @@ PREDICTION_THRES = 0.5  # confidence
 DBLEVEL_THRES = -30  # dB
 
 # Variables
-HOST = 'attu2.cs.washington.edu'
-PORT = 23401
+HOST = 'localhost'
+PORT = 28123
 CHANNELS = 1
 RATE = 16000
 CHUNK = RATE * 2
@@ -31,23 +31,23 @@ MICROPHONES_DESCRIPTION = []
 ###########################
 # Download model, if it doesn't exist
 ###########################
-MODEL_URL = "https://www.dropbox.com/s/cq1d7uqg0l28211/example_model.hdf5?dl=1"
-MODEL_PATH = "models/example_model.hdf5"
-print("=====")
-print("2 / 2: Checking model... ")
-print("=====")
-model_filename = "models/example_model.hdf5"
-homesounds_model = Path(model_filename)
-if (not homesounds_model.is_file()):
-    print("Downloading example_model.hdf5 [867MB]: ")
-    wget.download(MODEL_URL, MODEL_PATH)
+# MODEL_URL = "https://www.dropbox.com/s/cq1d7uqg0l28211/example_model.hdf5?dl=1"
+# MODEL_PATH = "models/example_model.hdf5"
+# print("=====")
+# print("2 / 2: Checking model... ")
+# print("=====")
+model_filename = "example_model.hdf5"
+# homesounds_model = Path(model_filename)
+# if (not homesounds_model.is_file()):
+#     print("Downloading example_model.hdf5 [867MB]: ")
+#     wget.download(MODEL_URL, MODEL_PATH)
 
 ##############################
 # Load Deep Learning Model
 ##############################
 print("Using deep learning model: %s" % (model_filename))
 model = load_model(model_filename)
-graph = tf.get_default_graph()
+graph = get_default_graph()
 
 ##############################
 # Setup Audio Callback
